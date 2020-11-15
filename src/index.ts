0ed9463691d4d2f36f0e279153e218c1306f76de
Matei Adriel <rafaeladriel11@gmail.com>
2020-11-15 10:50:13 +0200

    feat: added two basic validators for string lengths

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,19 @@
 // - The second arg is so validators can't return anything
 // - The last arg is so you can't pass stuff to iterator.next()
 export type Validator<E> = Generator<E, void, void>
+
+export function* tooShort(
+    min: number,
+    value: string,
+    message = `Expected string to have ${min} characters but it instead has ${value.length} characters.`
+): Validator<string> {
+    if (value.length < min) yield message
+}
+
+export function* tooLong(
+    max: number,
+    value: string,
+    message = `Expected string to have ${max} characters but it instead has ${value.length} characters.`
+): Validator<string> {
+    if (value.length > max) yield message
+}
